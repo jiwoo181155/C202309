c4bcdaa9157af076c6ab1e5b88942cab099563b6
jiwoo181155 <jwj9909@gmail.com>
2023-09-18 16:36:41 +0900

    Week3.1 반복문

--- challenge/Week3/do-while_even.c
+++ challenge/Week3/do-while_even.c
@@ -3,12 +3,13 @@
 int main(void) {
 	int i = 0;
 	do {
-		//i증감연산자
+		//i증감연산자-후위연산자 사용
 		i++;
+		//i가 짝수일 경우 print문을 실행하지않도록 조건문->i를 2로나누어 나머지가 0일 경우 짝수판별
 		if (i % 2 == 0) {
 			continue;
 		}
-		//수행할 문장
+		//수행할 문장-Hello World
 		printf("%d Hello World!\n", i);
 	} while (i < 10);
 	//while(조건식)
--- challenge/Week3/while_even.c
+++ challenge/Week3/while_even.c
@@ -2,14 +2,15 @@
 
 int main(void) {
 	int i = 0;
-	//while(조건식)
+	//i증감연산자-후위연산자 사용
 	while (i < 10) {
 		//i증감연산자
 		i++;
+		//i가 짝수일 경우 print문을 실행하지않도록 조건문->i를 2로나누어 나머지가 0일 경우 짝수판별
 		if (i % 2 == 0) {
 			continue;
 		}
-		//수행할 문장(증감연산 포함)
+		//수행할 문장-Hello World
 		printf("%d Hello World!\n", i);
 	}
 	return 0;
