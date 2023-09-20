30ad97710d4b3b19cd1a81c3ac3376c42299bc52
jiwoo181155 <jwj9909@gmail.com>
2023-09-20 16:11:54 +0900

    Week3.2.조건문 실습

---
+++ challenge/Week3/choiceIf.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+	int choice;
+	//입력값에 대한 출력가능 응답 제시
+	printf("1. 파일 저장\n");
+	printf("2. 저장 없이 닫기\n");
+	printf("3. 종료\n");
+	scanf_s("%d", &choice);
+	//관계연산자를 사용하여 choice값이 1이면 파일 저장을 출력(
+	if (choice == 1) {
+		printf("1. 파일 저장\n");
+	}
+	//관계연산자를 사용하여 choice값이 2이면 저장 없이 닫기를 출력
+	else if (choice == 2) {
+		printf("2. 저장 없이 닫기\n");
+	}
+	//관계연산자를 사용하여 choice값이 3이면 종료를 출력
+	else{
+		printf("3. 종료\n");
+	}
+	return 0;
+}
