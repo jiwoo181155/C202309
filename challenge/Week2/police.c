b9ad69a527a897ae67ae1deebd770666782e78e5
jiwoo181155 <jwj9909@gmail.com>
2023-10-31 23:10:05 +0900

    오류 수정

--- challenge/Week2/police.c
+++ challenge/Week2/police.c
@@ -1,22 +1,23 @@
-#include <stdio.h> //stdio.h 컴파일
+#include <stdio.h>  //컴파일러의 라이브러리에서 stdio.h 헤더파일을 찾아서 포함시킨다
 
+//main함수 시작
 int main(void) {
-	int age;
-	float weight;
-	float height;
-	char name[256];
-	char crime[256];
-	printf("이름?");
-	scanf_s("%s", name, sizeof(name));
-	printf("나이?");
-	scanf_s("%d", &age);
+	int age; //age변수 선언
+	float weight;  //weight 변수 선언
+	float height;   //height 변수 선언
+	char name[256];  //크기가 256인 문자열 배열 name 선언
+	char crime[256]; //크기가 256인 문자열 배열 crime 선언
+	printf("이름?");  
+	scanf_s("%s", name, sizeof(name));  //scanf_s문으로 입력받은 단어를 name배열에 저장
+	printf("나이?"); 
+	scanf_s("%d", &age);  //scanf_s문으로 입력받은 숫자를 int형 변수age에 저장
 	printf("몸무게?");
-	scanf_s("%f", &weight);
+	scanf_s("%f", &weight);  //scanf_s문으로 입력받은 숫자를 float형 변수weight에 저장
 	printf("키?");
-	scanf_s("%f", &height);
+	scanf_s("%f", &height); //scanf_s문으로 입력받은 숫자를 float형 변수height에 저장
 	printf("범죄?");
-	scanf_s("%s", crime, sizeof(crime));
-	printf("----범죄자정보----");
+	scanf_s("%s", crime, sizeof(crime)); //scanf_s문으로 입력받은 단어를 배열 crime에 저장
+	printf("----범죄자정보----");  //위에서 입력받아 저장한 값들을 한번에 출력해서 보여준다.
 	printf("이름: %s\n", name);
 	printf("나이: %d\n", age);
 	printf("몸무게: %f\n", weight);
--- challenge/Week3/choiceIf.c
+++ challenge/Week3/choiceIf.c
@@ -4,7 +4,7 @@ int main() {
 	int choice;
 	printf("1. 파일 저장\n");
 	printf("2. 저장 없이 닫기\n");
-	printf("3. 종료"\n);
+	printf("3. 종료\n");
 	//입력숫자 스캔문
 	scanf_s("%d", &choice);
 	//관계연산자==를 이용하여 chocie가 1이면 파일을 저장합니다 출력
@@ -17,7 +17,7 @@ int main() {
 	}
 	//관계연산자==를 이용하여 choice가 3이면 종료 출력
 	else if (choice == 3) {
-		printf("3. 종료"\n);
+		printf("3. 종료\n");
 	}
 	//그밖의 나머지 숫자는 잘못된 숫자 입력입니다를 출력
 	else {
